1f4c0abc929b37c1d6df1ecf03adf0704af8ccce
issa-clown <kkobayashi@iplab.cs.tsukuba.ac.jp>
2017-09-10 21:39:27 +0900

    merge from master

---
+++ Assets/Scripts/Search/LookAtObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtObject : MonoBehaviour {
+	[SerializeField]
+	GameObject _object;
+
+	[SerializeField]
+	Vector3 _lotate = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (_object.transform);
+		transform.Rotate (_lotate);
+	}
+}
