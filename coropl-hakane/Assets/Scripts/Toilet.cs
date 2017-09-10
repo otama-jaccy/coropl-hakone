507bd4dcd17b2a484a9dae6b8c059ebebc67485b
otama-jaccy <funamin310@gmail.com>
2017-09-10 14:23:31 +0900

    トイレ場所文字の作成

---
+++ coropl-hakane/Assets/Scripts/Toilet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Unity.Location;
+using Mapbox.Unity.Utilities;
+using Mapbox.Unity.Map;
+using UnityEngine.UI;
+
+public class Toilet : MonoBehaviour {
+	[SerializeField]
+	private AbstractMap _map;
+
+	[SerializeField]
+	private float lon,lat;
+
+	[SerializeField]
+	Button burst_button;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Conversions.GeoToWorldPosition(lon,lat,
+										_map.CenterMercator,
+										_map.WorldRelativeScale).ToVector3xz();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.name == "Player") {
+			Debug.Log ("BURST");
+			burst_button.SendMessage ("Visiable");
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.name == "Player") {
+			Debug.Log ("NOT BURST");
+			burst_button.SendMessage ("Hide");
+		}
+	}
+}
